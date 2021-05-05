981d843c2d37f2ada94c9f3ae8ee56478e236aa4
lewis <bcm.lewisbaek@gmail.com>
2021-05-05 18:05:52 +0900

    feat: swagger setting
    SwaggerConfig 파일 추가

---
+++ src/main/java/com/project/transaction/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.project.transaction.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+			.apiInfo(apiInfo())
+			.select()
+			.apis(RequestHandlerSelectors.basePackage("com.project.transaction"))
+			.paths(PathSelectors.any())
+			.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+			.title("Transaction History API")
+			.description("특정고객 거래내역 조회서비스")
+			.build();
+	}
+}
