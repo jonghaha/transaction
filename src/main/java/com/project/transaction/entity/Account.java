28f47163834d9538f7ada07e2a2dc10112c079c5
lewis <bcm.lewisbaek@gmail.com>
2021-05-06 23:47:28 +0900

    refactor(Entity): pk 생성전략을 DB에 위임
    - Transaction.java, Account.java

--- src/main/java/com/project/transaction/entity/Account.java
+++ src/main/java/com/project/transaction/entity/Account.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Account {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long accountSeq;
 	private Integer accountNumber;
 	private String accountName;
--- src/main/java/com/project/transaction/entity/Transaction.java
+++ src/main/java/com/project/transaction/entity/Transaction.java
@@ -21,7 +21,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Transaction {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long transactionSeq;
 
 	private LocalDate transactionDate;
